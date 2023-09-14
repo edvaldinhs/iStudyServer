61fd141b2f0e56130a0e3819cb802211d643761e
Rebeca Eduarda de Oliveira Santos <20211114010006@ifrn.local>
2023-09-14 16:04:59 -0300

    Primeiro Commit

---
+++ src/main/java/br/edu/ifrn/sc/info/iStudyServer/ConteudoWS.java
@@ -0,0 +1,27 @@
+package br.edu.ifrn.sc.info.iStudyServer;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.edu.ifrn.sc.info.iStudyServer.dao.ConteudoDAO;
+import br.edu.ifrn.sc.info.iStudyServer.dominio.Conteudo;
+
+@Path("conteudos")
+public class ConteudoWS {
+	
+	@GET
+	@Path("/listarTodos")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Conteudo> listarTodos() {
+		
+		System.out.println("Executando o método listar conteúdos");
+		ConteudoDAO dao = new ConteudoDAO();
+		return dao.listarTodos();
+		
+	}
+	
+}
---
+++ src/main/java/br/edu/ifrn/sc/info/iStudyServer/dominio/Disciplina.java
@@ -0,0 +1,33 @@
+package br.edu.ifrn.sc.info.iStudyServer.dominio;
+
+public class Disciplina {
+
+	private int id;
+	private String nome;
+	
+	public Disciplina() {
+		
+	}
+
+	public Disciplina(int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
